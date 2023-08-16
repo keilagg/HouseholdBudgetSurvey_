e33e67eca9edd76fb1883c055ff2714f5af2b26f
Keila G. Gómez <keilagonzalezgomez@hotmail.com>
2023-08-16 20:47:56 +0200

    feat():wip almost

--- code_random.py
+++ code_random.py
@@ -59,3 +59,23 @@ for year in df_dict_keys:
     transpExpense.append(expensesTrransp_yearly)
     totalExpense_perc.append(percentage_)
 plt.bar(df_dict_keys, totalExpense_perc );
+
+
+# Plot both DataFrames
+code_list_PT = ['code_07311', 'code_07313', 'code_07321']
+df_HBS_urban_nocar.loc[:,'public_transportation'] = df_HBS_urban_nocar[code_list_PT].sum(axis=1)
+df_HBS_urban_car.loc[:,'public_transportation'] = df_HBS_urban[code_list_PT].sum(axis=1)
+
+x = np.array(df_dict_keys, dtype = int)
+Y1 = np.array(df_HBS_urban_nocar.groupby(['ANOENC'])['public_transportation'].sum(), dtype = float)
+Y2 = np.array(df_HBS_urban_car.groupby(['ANOENC'])['public_transportation'].sum().to_list(), dtype = float)
+
+plt.bar(x - 0.2, Y1, 0.4,  label = 'Households without car')
+plt.bar(x + 0.2 , Y2, 0.4, label = 'Households that own a car')
+  
+#plt.xticks(X_axis, X)
+plt.xlabel("years")
+plt.ylabel("Total spent (€)")
+plt.title("Total spent each year in public transportation")
+plt.legend()
+plt.show();
--- helper_plot.py
+++ helper_plot.py
@@ -5,12 +5,12 @@ Created on Tue Aug 15 19:27:15 2023
 
 @author: keilagonzalez
 """
-
-
+import pandas as pd
+from matplotlib import pyplot as plt
 
 # Define function to plot
 
-def plot2print(b_colors:list, x:list, y:list, ylabel:str, title:str):
+def plot2print(x:list, y:list, b_colors:list, ylabel:str, title:str):
     fig, ax = plt.subplots()    
     
     # Style
@@ -48,3 +48,59 @@ def plot2print(b_colors:list, x:list, y:list, ylabel:str, title:str):
 
     # Better visualization
     fig.tight_layout()
+
+
+
+# plot share of income spent on car usage
+
+def scatter_plot(dict_df, year):
+# set bench values
+    transport_expenditure = 15 # 15% of total income spent in transportation
+    median_income = dict_df[year]['IMPEXAC'].median()
+
+    groups_list = []
+
+    #variables
+    x = dict_df[year]['IMPEXAC']
+    y = dict_df[year]['percentage_income']
+
+    dict_x_conditions = [x.where(x <= median_income), 
+                        x.where(x > median_income),
+                        x.where(x <= median_income),
+                        x.where(x > median_income)
+                    ]
+
+    dict_y_conditions = [y.where(y <= transport_expenditure), 
+                    y.where(y <= transport_expenditure),
+                    y.where(y > transport_expenditure),
+                    y.where(y > transport_expenditure)
+                    ]
+    fig, ax = plt.subplots()
+
+    colors = ['#fd8d3c', '#fecc5c', '#2c7fb8', '#253494']
+    legend_ = ['Low Income, Low Costs',
+            'Higher Income, Low Costs',
+            'Low Income, High Costs',
+            'Higher Income, High Costs'
+            ]
+    s = 10
+
+    # Plot
+    for i in range(len(colors)):
+                            ax.scatter(dict_x_conditions[i], 
+                            dict_y_conditions[i], 
+                            s = s,
+                            c = colors[i], 
+                            alpha = .3)
+    
+                            values_list = [dict_x_conditions[i], dict_y_conditions[i] ]
+                            groups_list.append(values_list)
+
+    # Benchmark Mean values          
+    ax.axhline(y = transport_expenditure, color = 'red', linewidth=1)           
+    ax.axvline(x = median_income, color = 'red', linewidth = 1);
+
+    ax.legend(legend_)
+    plt.title(year)
+    plt.show()
+    return groups_list
